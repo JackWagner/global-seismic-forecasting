f3a6d6c957160c7128a9b23c9efb302d971039e5
Jack Wagner <jwwagner@ucsc.edu>
2022-02-21 11:55:51 -0800

    Parses through any Global Catalog of Calibrated Earthquake Locations (GCCEL) data file passed via command line argument, column oriented storage

---
+++ gccel_parse.py
@@ -0,0 +1,85 @@
+import time
+import re 
+import sys
+
+
+
+#Open and read Global Catalog of Calibrated Earthquake Locations (GCCEL) data file
+gccel_file = open(str(sys.argv[1]),"r",encoding="utf8")
+gccel_data = gccel_file.read()
+
+
+print(str(sys.argv[1])," opened")
+time.sleep(2)
+
+gccel_file.close()
+
+#Regex for array of records
+records = re.split("STOP",gccel_data)
+num_of_records = len(records)
+
+print("Number of Records: ", str(num_of_records), "\n", "Lines per record: ", str(records[1].count("\n")))
+
+#column-oriented storage of event, hypocenter and magnitude records
+#initializing arrays with zeros
+event_record = [None]*num_of_records             #Event ID 5:121
+
+hypocenter_datetime_record = [None]*num_of_records   #Datetime Y=5:8, M=10:11, D=13:14, H=16:17, m=19:20, s 22:26
+
+hypocenter_latitude_record = [None]*num_of_records   #Latitude 35:42
+hypocenter_longitude_record = [None]*num_of_records  #Longitude 44:52
+
+hypocenter_clusterID_record = [None]*num_of_records  #ClusterID 104:121
+
+magnitude_surface_record = [None]*num_of_records     #Magnitude 5:8, Scale=MS at 10:14
+magnitude_body_record = [None]*num_of_records        #Magnitude 5:8, Scale=mb at 10:14
+magnitude_richter_record = [None]*num_of_records     #Magnitude 5:8, Scale=ML at 10:14
+magnitude_moment_record = [None]*num_of_records      #Magnitude 5:8, Scale=MW at 10:14
+
+if(len(sys.argv) == 2 or (int(sys.argv[2]) == 0 and int(sys.argv[3]) == 0)):
+    record_range = range(num_of_records)
+else:
+    record_range = range(int(sys.argv[2]),int(sys.argv[3]))
+print(str(records[1].count("\n")))
+for i in record_range: 
+    temp_records = records[i].splitlines()
+    
+    for j in range(len(temp_records)):
+        line_num = i*records[1].count("\n")+j+1 
+        if (temp_records[j][0:2] == "E "):
+            print("\n\n  Event:     ", temp_records[j][4:101])
+            event_record[i] = temp_records[j][4:101]
+        
+        if (temp_records[j][0:2] == "H "):
+            
+            hypocenter_datetime_record[i] = temp_records[j][4:26]
+            hypocenter_latitude_record[i] = float(temp_records[j][34:42])
+            hypocenter_longitude_record[i] = float(temp_records[j][43:52])
+            hypocenter_clusterID_record[i] = temp_records[j][103:121]
+
+            print("  Datetime:     ", temp_records[j][4:26])
+            print("  Latitude/Longitude:    ", temp_records[j][34:42]," / ",temp_records[j][43:52])
+            print("  ClusterID:    ",temp_records[j][103:121])
+
+        if (temp_records[j][0:2] == "M "):
+            
+            if (temp_records[j][9:11] == "MS"):
+                magnitude_surface_record[i] = float(temp_records[j][4:8])
+                print("  Surface Magnitude:    ", temp_records[j][4:8])
+            
+            if (temp_records[j][9:11] == "mb"):
+                magnitude_body_record[i] = float(temp_records[j][4:8])
+                print("  Body Magnitude:    ", temp_records[j][4:8])
+            
+            if (temp_records[j][9:11] == "ML"):
+                magnitude_richter_record[i] = float(temp_records[j][4:8])
+                print("  Richter Magnitude:   ", temp_records[j][4:8])
+            
+            if (temp_records[j][9:11] == "MW" or temp_records[j][9:11] == "Mw"):
+                magnitude_moment_record[i] = float(temp_records[j][4:8])
+                print("  Moment Magnitude:   ", temp_records[j][4:8])
+
+print("End of File")
+
+
+
