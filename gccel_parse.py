1cb95df8bc123ad99fcd1154ba676ddb7ca1bbf0
Jack Wagner <jwwagner@ucsc.edu>
2022-02-22 12:19:22 -0800

    import pandas and moved data into dataframe for export to earthquakes.csv, trimmed whitespace in clusterID

--- gccel_parse.py
+++ gccel_parse.py
@@ -1,7 +1,7 @@
 import time
 import re 
 import sys
-
+import pandas as pd
 
 
 #Open and read Global Catalog of Calibrated Earthquake Locations (GCCEL) data file
@@ -9,7 +9,7 @@ gccel_file = open(str(sys.argv[1]),"r",encoding="utf8")
 gccel_data = gccel_file.read()
 
 
-print(str(sys.argv[1])," opened")
+print(str(sys.argv[1]),"opened")
 time.sleep(2)
 
 gccel_file.close()
@@ -18,7 +18,7 @@ gccel_file.close()
 records = re.split("STOP",gccel_data)
 num_of_records = len(records)
 
-print("Number of Records: ", str(num_of_records), "\n", "Lines per record: ", str(records[1].count("\n")))
+print("Number of Records: ", str(num_of_records), "\n")
 
 #column-oriented storage of event, hypocenter and magnitude records
 #initializing arrays with zeros
@@ -47,7 +47,7 @@ for i in record_range:
     for j in range(len(temp_records)):
         line_num = i*records[1].count("\n")+j+1 
         if (temp_records[j][0:2] == "E "):
-            print("\n\n  Event:     ", temp_records[j][4:101])
+            #print("\n\n  Event:     ", temp_records[j][4:101])
             event_record[i] = temp_records[j][4:101]
         
         if (temp_records[j][0:2] == "H "):
@@ -55,31 +55,39 @@ for i in record_range:
             hypocenter_datetime_record[i] = temp_records[j][4:26]
             hypocenter_latitude_record[i] = float(temp_records[j][34:42])
             hypocenter_longitude_record[i] = float(temp_records[j][43:52])
-            hypocenter_clusterID_record[i] = temp_records[j][103:121]
+            hypocenter_clusterID_record[i] = temp_records[j][103:121].strip()
 
-            print("  Datetime:     ", temp_records[j][4:26])
-            print("  Latitude/Longitude:    ", temp_records[j][34:42]," / ",temp_records[j][43:52])
-            print("  ClusterID:    ",temp_records[j][103:121])
+            #print("  Datetime:     ", temp_records[j][4:26])
+            #print("  Latitude/Longitude:    ", temp_records[j][34:42]," / ",temp_records[j][43:52])
+            #print("  ClusterID:    ",temp_records[j][103:121])
 
         if (temp_records[j][0:2] == "M "):
             
             if (temp_records[j][9:11] == "MS"):
                 magnitude_surface_record[i] = float(temp_records[j][4:8])
-                print("  Surface Magnitude:    ", temp_records[j][4:8])
+                #print("  Surface Magnitude:    ", temp_records[j][4:8])
             
             if (temp_records[j][9:11] == "mb"):
                 magnitude_body_record[i] = float(temp_records[j][4:8])
-                print("  Body Magnitude:    ", temp_records[j][4:8])
+                #print("  Body Magnitude:    ", temp_records[j][4:8])
             
             if (temp_records[j][9:11] == "ML"):
                 magnitude_richter_record[i] = float(temp_records[j][4:8])
-                print("  Richter Magnitude:   ", temp_records[j][4:8])
+                #print("  Richter Magnitude:   ", temp_records[j][4:8])
             
             if (temp_records[j][9:11] == "MW" or temp_records[j][9:11] == "Mw"):
                 magnitude_moment_record[i] = float(temp_records[j][4:8])
-                print("  Moment Magnitude:   ", temp_records[j][4:8])
+                #print("  Moment Magnitude:   ", temp_records[j][4:8])
 
-print("End of File")
+
+data = { 'ClusterID' : hypocenter_clusterID_record, 'Datetime' : hypocenter_datetime_record,
+         'Latitude' : hypocenter_latitude_record, 'Longitude' : hypocenter_longitude_record,
+         'Surface Magnitude' : magnitude_surface_record, 'Body Magnitude' : magnitude_body_record,
+         'Richter Magnitude' : magnitude_richter_record, 'Moment Magnitude' : magnitude_moment_record }
+
+earthquake_df = pd.DataFrame(data)
+
+earthquake_df.to_csv("earthquakes.csv",index=False,mode='w')
 
 
 
